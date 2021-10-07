69bf21b798ae2237644849d6dba1056645175391
RzRaul <rodriguez.raul54@uabc.edu.mx>
2021-10-06 23:51:02 -0700

    First commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, CimaWorld!</Text>
+      <Text>Hello, CimaWorld!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
