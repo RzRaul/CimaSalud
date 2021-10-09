6c5af90e6318fcc55f4cba0d8288e493b43b684f
RzRaul <rodriguez.raul54@uabc.edu.mx>
2021-10-08 23:22:26 -0700

    se agregó módulo de navegación para cambiar entre pantallas]

---
+++ screens/Inicio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Inicio () => {
+    return (
+        <View style = {}>
+            <Text>  Home view </Text>
+        </view>
+
+    )
+}
