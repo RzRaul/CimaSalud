0a7f199b03b890cf021549bb54b1c515bee21002
adlai <adlai.carmona@uabc.edu.mx>
2021-11-03 22:06:37 -0700

    + Added Log In screen with image and handlers

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,23 +1,46 @@
-import React from 'react';
-import { Button, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { ImageBackground, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import styles from '../styles/styles';
 import { AuthContext } from '../utils/AuthContext';
 
 const Login = ({navigation}) => {
     const {signIn} = React.useContext(AuthContext);
+    const {email, setEmail} = React.useState(null);
+    const {pass, setPass} = React.useState(null);
+
     return (
-        <View style = {styles.container}>
-            <Text>  This is LoginScreen </Text>
-            <Button 
-                title='Iniciar sesión'
-                color = '#1779ba'
-                onPress = {()=> signIn('rulas@test.com', 'pass123')}
-            />
-            <Button 
-                title='Crear Cuenta'
-                color = '#1779ba'
-                onPress = {()=> navigation.push('SignUp')}
-            />
+        <View style = {{
+            flex: 1,
+            backgroundColor: '#d4c9b9',
+            alignItems: 'center',
+            justifyContent: 'center',
+            }}>
+            <ImageBackground source={require('../assets/loginBackground.png')} resizeMode="cover" style={{flex: 1, width: 300, justifyContent:'flex-end', alignItems:'center'}}>
+                <TextInput 
+                    style = {[styles.input, {width:200}]}
+                    placeholder="Enter e-mail"
+                    onChangeText={setEmail}  
+                />
+                <TextInput 
+                    style = {[styles.input, {width:200}]}
+                    placeholder="Enter password"
+                    onChangeText = {setPass}
+                />
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress = {()=> signIn('rulas@test.com', 'pass123')}
+                >
+                    
+                    <Text>LOG IN</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress = {()=> navigation.push('SignUp')}
+                >
+                    <Text>SIGN UP</Text>
+                </TouchableOpacity>
+            </ImageBackground>
+            
         </View>
     )
 }
--- screens/MoreScreen.js
+++ screens/MoreScreen.js
@@ -1,19 +1,48 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import { Alert, Button, ScrollView, Text, View } from 'react-native';
 import {Calendar} from 'react-native-calendars';
 
 import { AuthContext } from '../utils/AuthContext';
+import * as DayFuncs from '../services/dayFetchs';
 import styles from '../styles/styles';
 
 const More = ({navigation}) => {
     //const {state, getUserInfo} = useContext(Context);
-    const {signIn} = React.useContext(AuthContext);
+
+    // Get User token and if null login again
+    const {getState} = React.useContext(AuthContext);
+    const token = 1;
+    //const token = getState().userToken;
+    if(!token){
+        console.log('token is null');
+        //navigation.navigate('Login')
+    }
+    const [foods,setFoods] = useState([{
+        name:'pizza',
+        grasas:25,
+        proteinas:20,
+        cals:100,
+        carbs:30
+    },{
+        name:'hamburguesa',
+        grasas:30,
+        proteinas:25,
+        cals:150,
+        carbs:50
+    }]);
+    try {
+        //setFoods(DayFuncs.getDayByDate(token,Date()));
+    } catch (error) {
+        console.log("Error at line 21: 'setFoods(DayFuncs.getDayByDate(token,Date()));'");
+        setFoods();
+    }
     
+
+    //Cuando se tenga funcion para recibir los dias con datos cambiar esto
     const markedDates = {
-        '2021-11-16': {selected: true, marked: true, selectedColor: 'blue'},
+        '2021-11-16': {marked: true},
         '2021-11-17': {marked: true},
-        '2021-11-18': {marked: true, dotColor: 'red', activeOpacity: 0},
-        '2021-11-19': {disabled: true, disableTouchEvent: true}
+        '2021-11-18': {marked: true},
     }
 
     const createTwoButtonAlert = () =>
@@ -30,18 +59,47 @@ const More = ({navigation}) => {
       ]
     );
 
+    const ListItem = ({food}) => {
+        return (
+            <View style={{paddingTop: 15, backgroundColor: '#F3F3F3'}}>
+                <View style={{padding: 15, backgroundColor: '#9DD5D4'}}>
+                    <View style = {{flexDirection: 'row', justifyContent: 'space-between'}}>
+                        <Text style = {styles.textBody}>{food.name}</Text>
+                    </View>
+                    <View style = {{padding: 10, flexDirection: 'column', justifyContent: 'space-around'}}>
+                        <Text style = {styles.textBody}>Calorias {food.cals}</Text>
+                        <Text style = {styles.textBody}>Grasas {food.grasas}</Text>
+                        <Text style = {styles.textBody}>Proteinas {food.proteinas}</Text>
+                        <Text style = {styles.textBody}>Carbohidratos {food.carbs}</Text>
+                    </View>
+                </View>
+            </View>
+        );
+    };
+
     return (
         <View style = {styles.container}>
-            <ScrollView>
+            <ScrollView style = {{margin: 5, paddingTop:10}}>
                 <Calendar 
-                    onDayPress={(day) => {console.log('selected day', day)}}
+                    onDayPress={(date) => {
+                        console.log('selected day', date);
+                        //const {dateString,day,month,year} = date;
+                        //DayFuncs.getDayByDate(token,dateString,day,month,year)
+                        delete date.timestamp;
+                        setFoods(DayFuncs.getDayByDate(token,date));
+                    }}
                     markedDates = {markedDates}
                 />
-                <Button 
-                    title='Contactanos'
-                    color = '#1779ba'
-                    onPress = {createTwoButtonAlert}
-                />
+                <View>
+                    {foods.map((food,i) => (<ListItem food={food} key={i}/>))}
+                </View>
+                <View style = {{margin: 5, paddingTop:10}}>
+                    <Button 
+                        title='Contactanos'
+                        color = '#1779ba'
+                        onPress = {createTwoButtonAlert}
+                    />
+                </View>
             </ScrollView>
         </View>
     )
